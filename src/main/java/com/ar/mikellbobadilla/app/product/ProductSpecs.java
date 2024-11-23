8d1d84863aeed48536e9118424d17b87997dc3b9
Sting <bobadillasting@gmail.com>
2024-11-22 23:01:23 -0300

    Fix(Product): Parse correctly to integer if is

--- src/main/java/com/ar/mikellbobadilla/app/product/ProductSpecs.java
+++ src/main/java/com/ar/mikellbobadilla/app/product/ProductSpecs.java
@@ -7,20 +7,31 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class ProductSpecs {
 
-    public static Specification<Product> hasNameLike(String productName) {
+    public static Specification<Product> hasId(String id) {
         return (root, query, builder) -> {
-            if (productName == null || productName.isEmpty()) {
+            if (id == null || id.isEmpty()) return null;
+            try {
+                Integer.parseInt(id);
+            } catch (NumberFormatException ignore) {
                 return null;
             }
+
+            return builder.equal(root.get("id"), id);
+        };
+    }
+
+    public static Specification<Product> hasNameLike(String productName) {
+        return (root, query, builder) -> {
+            if (productName == null || productName.isEmpty()) return null;
+
             return builder.like(builder.lower(root.get("name")), "%" + productName.toLowerCase() + "%");
         };
     }
 
     public static Specification<Product> hasCategoryLike(String categoryName) {
         return (root, query, builder) -> {
-            if (categoryName == null || categoryName.isEmpty()) {
-                return null;
-            }
+            if (categoryName == null || categoryName.isEmpty()) return null;
+
             Join<Product, Category> categoryJoin = root.join("categories", JoinType.LEFT);
             return builder.like(builder.lower(categoryJoin.get("name")), "%" + categoryName.toLowerCase() + "%");
         };
